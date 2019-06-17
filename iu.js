b4cc32959978394df7e3a84f886629c4693bafd4
muherezaamon <muherezaamon75@gmail.com>
2019-06-17 15:31:56 +0300

    copied iu.js file in number-guessing-game folder2

---
+++ iu.js
@@ -0,0 +1,19 @@
+var i = Math.floor(Math.random() * 100) + 1;
+
+var j=i%2; //gets the modulus and stores the result in 
+if (j==0) //
+    console.log(i +" is Evn");
+else{
+    console.log(i +" is odd");
+}
+for  (count=1; count<=10;count++);
+{
+	let num = prompt ("plesea enter your number");
+	if (num > 0);
+	console.log(1)
+
+if (num > i)
+console.log("Number you have chosen is higher, try another");
+else if (num < i)
+console.log("Number you have chosen is lower, try anther")
+}
